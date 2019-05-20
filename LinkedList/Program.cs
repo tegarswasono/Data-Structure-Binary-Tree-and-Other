09fdea0de7b34361d9e88ee7554f382d9f8d6aa1
Tegar Swasono <tegarswasono123@gmail.com>
2019-05-20 21:03:23 +0700

    singly linked list

---
+++ LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singly s = new Singly();
+            s.insertFirst(3);
+            s.insertFirst(5);
+            s.insertFirst(7);
+            s.insertFirst(9);
+            s.insertFirst(11);
+            s.insertFirst(13);
+            s.deleteFirst();
+
+            var result = s.find(7);
+            var result2 = s.delete(5);
+        }
+    }
+}
---
+++ LinkedList/Singly.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Link
+    {
+        public int data;
+        public Link next;
+        public Link(int data)
+        {
+            this.data = data;
+        }
+        public void displayLink()
+        {
+            Console.WriteLine(data+" ");
+        }
+    }
+    class Singly
+    {
+        private Link first;
+        public Singly()
+        {
+            first = null;
+        }
+        public void insertFirst(int data)
+        {
+            Link newlink = new Link(data);
+            newlink.next = first;
+            first = newlink;
+        }
+        public void deleteFirst()
+        {
+            first = first.next;
+        }
+
+        public bool find(int key)
+        {
+            Link current = first;
+            while (current.data!=key)
+            {
+                if (current.next==null)
+                {
+                    return false;
+                }else
+                {
+                    current = current.next;
+                }
+            }
+            return true;
+        }
+        public bool delete(int key)
+        {
+            Link current = first;
+            Link previous = first;
+            while (current.data!=key)
+            {
+                if (current.next==null)
+                {
+                    return false;
+                }else
+                {
+                    previous = current;
+                    current = current.next;
+                }
+            }
+            if (current==first)
+            {
+                first = first.next;
+            }else
+            {
+                previous.next = current.next;
+            }
+            return true;
+        }
+    }
+}
