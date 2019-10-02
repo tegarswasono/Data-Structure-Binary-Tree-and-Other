715e432d185e456258c44a5b70385f4dbc08913a
tegarsweefer <tegar.s@weefer.co.id>
2019-10-02 14:11:18 +0700

    binary tree

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -24,6 +24,81 @@ namespace BinaryTree
             theTree.insert(87, "Orin");
             theTree.insert(93, "Wiwid");
             theTree.insert(97, "Sasa");
+
+            while (true)
+            {
+                Console.Write("Enter first letter of show, "+ "insert, find, delete, or traverse: ");
+                int choice = getChar();
+                switch (choice)
+                {
+                    case 's':
+                        theTree.displayTree();
+                        break;
+                    case 'i':
+                        Console.Write("Enter value and data to"+ " insert: ");
+                        value = getInt();
+                        data = getString();
+                        theTree.insert(value, data);
+                        break;
+                    case 'f':
+                        Console.Write("Enter value to find: ");
+                        value = getInt();
+                        Node found = theTree.find(value);
+                        if (found != null)
+                        {
+                            Console.Write("Found: ");
+                            found.displayNode();
+                            Console.Write("\n");
+                        }
+                        else
+                        {
+                            Console.Write("Could not find "+ value);
+                        }
+                        break;
+                    case 'd':
+                        Console.Write("Enter value to delete: ");
+                        value = getInt();
+                        bool didDelete = theTree.delete(value);
+                        if (didDelete)
+                        {
+                            Console.Write("Deleted " + value);
+                        }
+                        else
+                        {
+                            Console.Write("Could not delete "+ value);
+                        }
+                        break;
+                    case 't':
+                        Console.Write("Enter type 1, 2 or 3: ");
+                        value = getInt();
+                        theTree.traverse(value);
+                        break;
+                    default:
+                        Console.Write("Invalid entry ");
+                        break;
+                }
+            }
+        }
+        public static char getChar()
+        {
+            String s = getString();
+            var tmp = s.ToCharArray();
+            char result = 'z';
+            if (tmp.Count()>0)
+            {
+                result = tmp[0];
+            }
+            return result;
+        }
+        public static String getString()
+        {
+            string s = Console.ReadLine();
+            return s;
+        }
+        public static int getInt()
+        {
+            String s = getString();
+            return int.Parse(s);
         }
     }
 }
